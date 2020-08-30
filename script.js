899bb9cc42f1a1e8ca678ddc0a7296f50821fefa
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-08-30 10:51:15 -0400

    joinfunction options, added support for function as object

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
 "use strict";
 
-function joinFunction(...funcs) {
-	function newFunc(...i) {
-		for (let f of funcs)
-			f.apply(this, i);
+function joinFunction(options, ...funcs) {
+	if (typeof options != 'object') {
+		funcs.unshift(options);
+		options = {};
 	}
-	newFunc.prototype = funcs[0].prototype;
-	return newFunc;
-}
+	options = Object.assign(options, {
+		prototype: 0,
+	});
 
-function joinFunctionAndPrototype(...funcs) {
-	function recursiveProtoJoin(protos) {
-		if (protos.length)
-			return Object.assign(
-				Object.create(recursiveProtoJoin(
-					protos.filter(e => e).map(e => Object.getPrototypeOf(e))
-				)),
-				...protos);
-		else
-			return null;
-	}
-
-	function newFunc(...i) {
+	function newFunc() {
 		for (let f of funcs)
-			f.apply(this, i);
+			f.apply(this, arguments);
 	}
-	newFunc.prototype = recursiveProtoJoin(funcs.map(e => e.prototype));
+	newFunc = Object.assign(newFunc, ...funcs);
+	if (options.prototype === 'join') {
+		function recursiveProtoJoin(protos) {
+			if (protos.length)
+				return Object.assign(
+					Object.create(recursiveProtoJoin(
+						protos.filter(e => e).map(e => Object.getPrototypeOf(e))
+					)),
+					...protos);
+			else
+				return null;
+		}
+		newFunc.prototype = recursiveProtoJoin(funcs.map(e => e.prototype));
+	} else if (typeof options.prototype == 'number')
+		newFunc.prototype = funcs[options.prototype].prototype;
+	else
+		newFunc.prototype = options.prototype;
 	return newFunc;
 }
 
 function overwriteFunction(a, b) {
+	b = Object.assign(b, a);
 	b.prototype = Object.assign(a.prototype, b.prototype);
 	return b;
 }
 
 function disableFunction(a) {
 	b = function () { };
+	for (let i in a)
+		b[i] =
+			typeof a[i] == 'function' ?
+				function () { } :
+				a[i];
 	for (let i in a.prototype)
 		b.prototype[i] =
 			typeof a.prototype[i] == 'function' ?
